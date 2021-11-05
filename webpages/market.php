f2de82d1b0d6ad17d6e9e00d987e622c2cdbeb4c
Priyank <pp677@njit.edu>
2021-11-05 16:47:59 -0400

    market page

--- webpages/market.php
+++ webpages/market.php
@@ -1,33 +1,67 @@
- <!DOCTYPE html>
-<html>
-<head>
-<title>Title of the document</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<!doctype html>
+<html lang="en">
 
-<link rel="stylesheet" type="text/css" href="marketStyle.css">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style>
+    #ques {
+        min-height: 433px;
+    }
+
+    .container-1 {
+        min-height: 433px;
+        margin-left: 75px;
+    }
+
+    .posting {
+        margin-left: 75px;
+    }
+    </style>
+    <title>Thread</title>
+
 </head>
 
 <body>
-<div class="card mb-3">
-  <div class="row g-5">
-    <div class="col-md-5">
+    <?php include 'header.php';
+            include 'connect.php';
+            $id = $_GET['tID'];
+            $sql = "SELECT * FROM `carssells` WHERE carID=$id";
+            $result = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+                $carName = $row['carName'];
+                $carDesc = $row['carDesc'];
+                $dealer = $row['ownerName'];
+                $contact = $row['contactInfo'];
+                echo'<div class="card mb-3">
+                <img src="https://source.unsplash.com/1500x300/?f1" style="window.screen.width;" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">'.$carName.'</h5>
+                    <p class="card-text">'.$carDesc.'</p>
+                    <p class="card-text"><small class="text-muted"><b>Dealer Name: </b>'.$dealer.'<b> Contact: </b>'.$contact.'</small></p>
+                </div>
+            </div>';
+            }
+        ?>
 
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-	
-      <img src="/webpages/images/car.jpeg" class="img-fluid-rounded-start" alt="Responsive image">
-        <h5 class="card-title">Car 1</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+    <?php include 'footer.php';?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
 </body>
 
-</html> 
+</html>
