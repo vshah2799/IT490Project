e1fa1e8724c03efad13673b81146b2222d6f273d
vshah <vshah2799@gmail.com>
2021-11-04 22:43:21 -0400

    Made userLoginRabbitMQServer.php use prepared statements

--- rabbitMQFiles/userLoginRabbitMQServer.php
+++ rabbitMQFiles/userLoginRabbitMQServer.php
@@ -43,6 +43,9 @@ function requestProcessor($request)
 
     $userID = $request['userID'];
     $password = $request['password'];
+
+
+    /*
     $sql = "SELECT password FROM users WHERE (userID = '$userID')";
     $results = mysqli_query($conn, $sql);
     if (mysqli_num_rows($results) == 1){
@@ -57,6 +60,22 @@ function requestProcessor($request)
 	    return true;
     }
     return false;
+    */
+
+    $selectStmt = $conn->prepare("SELECT password FROM users WHERE (userID = ?)");
+    $selectStmt->bind_param("s", $userID);
+    $selectStmt->execute();
+    $result = $selectStmt->get_result();
+    $passwordText = $result->fetch_assoc();
+
+    if(!empty($passwordText)){
+        if(password_verify($password, $passwordText['password'])){
+            return true;
+        }
+    }
+
+    return false;
+
 }
 
 $server = new rabbitMQServer("loggingRabbitMQ.ini","testServer");
