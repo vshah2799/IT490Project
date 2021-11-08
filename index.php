098cb4b4e2d8550745c6c32ed82d203438c09092
vshah <vshah2799@gmail.com>
2021-11-07 21:02:07 -0500

    Updated file paths on index.php

--- index.php
+++ index.php
@@ -16,25 +16,28 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="/webpages/recalls.php">Check Recall</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="~/Desktop/IT490Project/webpages/forums.php">Forums</a>
-	</li>
-	<li class="nav-item">
-          <a class="nav-link" href="./webpages/signIn.php">Account</a>
+          <a class="nav-link" href="/webpages/thread.php">Forums</a>
+	    </li>
+	    <li class="nav-item">
+          <a class="nav-link" href="/webpages/signIn.php">Account</a>
         </li>
+
+
       </ul>
     </div>
   </div>
 </nav>
 
- <a href="/webpages/forums.php">Forums2</a>  
 
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 			<script>
