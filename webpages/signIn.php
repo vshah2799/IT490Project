549af338a165ab4d371ddf84b17804faaf1c5c0a
vshah <vshah2799@gmail.com>
2021-11-09 21:06:35 -0500

    Added frontend code to signIn files. Now workring without the actual sql calls

--- webpages/signIn.php
+++ webpages/signIn.php
@@ -8,7 +8,7 @@
 
 		</head>
 <div class="container-lg">
-    <form class="row g-3" method="post" action="test.php">
+    <form class="row g-3" method="post" action="signInNext.php">
         <div class="col-md-4">
             <label id="userName" for="make" class="form-label">Username</label>
             <input type="text" class="form-control" id="userName" name="userName" required>
@@ -28,24 +28,11 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-<<<<<<< HEAD
-
 	 	</body>
 <script>
-=======
-<?php
-//global $USERNAME = $_POST["userName"];
-//global $PASSWORD = $_POST["passWord"];
-?>
-    <script>
 
-				sessionStorage.setItem('user', '');
-				sessionStorage.setItem('pass', '');
->>>>>>> Aaron
-
-    var userName = document.getElementById("userName").value;
-    sessionStorage.setItem('user', userName);
+    sessionStorage.setItem('user', '');
     sessionStorage.setItem('pass', '';
 
-</script>
-	</html>
+    </script>
+</html>
