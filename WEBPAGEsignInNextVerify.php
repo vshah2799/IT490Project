2df8d18f703a7e3b56b740bc5d5d30f620dcd5fd
Vishal <vshah2799@gmail.com>
2021-11-10 19:33:28 -0500

    sessionstorage error being worked on

--- WEBPAGEsignInNextVerify.php
+++ WEBPAGEsignInNextVerify.php
@@ -9,6 +9,9 @@
 <script>
 	tempUser = sessionStorage.getItem("user");
 	tempPass = sessionStorage.getItem("pass");
+
+	alert(tempUser);
+	alert(tempPass);
 </script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -19,8 +22,8 @@
 $user = "<script>document.writeln(tempUser);</script>";
 $pass = "<script>document.writeln(tempPass);</script>";
 
-alert($user);
-alert($pass):
+echo $user;
+echo $pass;
 
 $user = "vTest";
 $pass = "vTest";
@@ -30,7 +33,7 @@ $result = shell_exec("php RABBITMQuserLoginRabbitMQClient.php $user $pass");
 if ($result)
 {
 	echo "SUCCESS, SIGNED IN";
-	header("refresh: 2; url=WEBPAGEshowAccountDetails.php");
+	header("refresh: 10; url=WEBPAGEshowAccountDetails.php");
 }
 else
 {
