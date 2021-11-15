1d02c401d9595d07f848e4a8d6464bb0ba8f2e77
vshah <vshah2799@gmail.com>
2021-11-14 23:19:47 -0500

    Updated showaccountdetailsnest so outputting data can actually be shown

--- WEBPAGEshowAccountDetailsNext.php
+++ WEBPAGEshowAccountDetailsNext.php
@@ -13,19 +13,21 @@ $user = $_POST['user'];
 print($user);
 $query = shell_exec("php RABBITMQshowUserDataRabbitMQClient.php test");
 
-$username = $query['userID'];
-$email = $query['email'];
-$firstname = $query['firstname'];
-$lastname = $query['lastname'];
-$address = $query['address'];
-$make = $query['make'];
-$model = $query['model'];
-$year = $query['year'];
+print("Username: ". $query['userID']);
+print("Email " . $query['email']);
+print("Firstname " . $query['firstname']);
+print("Lastname " . $query['lastname']);
+print("Address " . $query['address']);
+print("Make " . $query['make']);
+print("Model " . $query['model']);
+print("Year " . $query['year']);
 if($query['recallFixed'] == 1){
-    $recallFixed = "Yes";
+    print("Recall fixed: " . $recallFixed = "Yes");
 }else{
-    $recallFixed = "No";
+    print("Recall fixed: " . $recallFixed = "No");
 }
+
+
 ?>
 
 
@@ -45,46 +47,6 @@ if($query['recallFixed'] == 1){
 
 <a class="btn btn-primary" href="WEBPAGEupdateAccountDetails.php" role="button">Update Account Details</a>
 
-<div class="container-lg">
-    <form class="row g-3" method="post" action="">
-        <div class="col-md-4">
-            <label for="make" class="form-label">Username</label>
-            <input type="text" readonly class="form-control" id="username" name="username" required value=<?php echo $username?>>
-        </div>
-        <div class="col-md-4">
-            <label for="validationDefault02" class="form-label">Email</label>
-            <input type="text" readonly class="form-control" id="email" name="email" required value=<?php echo $email?>>
-        </div>
-        <div class="col-md-4">
-            <label for="validationDefaultUsername" class="form-label">Firstname</label>
-            <input type="text" readonly class="form-control" id="firstname" name="firstname"  aria-describedby="inputGroupPrepend2" required value=<?php echo $firstname?>>
-        </div>
-        <div class="col-md-4">
-            <label for="validationDefaultUsername" class="form-label">Lastname</label>
-            <input type="text" readonly class="form-control" id="lastname" name="lastname"  aria-describedby="inputGroupPrepend2" required value=<?php echo $lastname?>>
-        </div>
-        <div class="col-md-4">
-            <label for="validationDefaultUsername" class="form-label">Address</label>
-            <input type="text" readonly class="form-control" id="address" name="address"  aria-describedby="inputGroupPrepend2" required value=<?php echo $address?>>
-        </div>
-        <div class="col-md-4">
-            <label for="validationDefaultUsername" class="form-label">Make</label>
-            <input type="text" readonly class="form-control" id="make" name="make"  aria-describedby="inputGroupPrepend2" value=<?php echo $make?>>
-        </div>
-        <div class="col-md-4">
-            <label for="validationDefaultUsername" class="form-label">Model</label>
-            <input type="text" readonly class="form-control" id="model" name="model"  aria-describedby="inputGroupPrepend2" value=<?php echo $model?>>
-        </div>
-        <div class="col-md-4">
-            <label for="validationDefaultUsername" class="form-label">Year</label>
-            <input type="text" readonly class="form-control" id="year" name="year"  aria-describedby="inputGroupPrepend2" value=<?php echo $year?>>
-        </div>
-        <div class="col-md-4">
-            <label for="validationDefaultUsername" class="form-label">Is your car's recall issue fixed?</label>
-            <input type="text" readonly class="form-control" id="recallFixed" name="recallFixed"  aria-describedby="inputGroupPrepend2" value=<?php echo $recallFixed?>>
-        </div>
-    </form>
-</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
